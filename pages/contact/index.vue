80288c9c9972342fd8f298269e5f7e83115c98b3
Monika Mráziková <monika.monika.mrazikova@gmail.com>
2023-05-15 22:38:09 +0200

    Created form view for sending message in contact page

---
+++ pages/contact/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <form>
+    <div id="page">
+      <div>
+        <label for="email">Your Email Adress *</label>
+        <input type="text" name="email" id="email" autocomplete="email" 
+          class="w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300" 
+        />
+      </div>
+      <div>
+        <label for="email">Retype Your Email Adress *</label>
+        <input type="text" name="email" id="email" autocomplete="email" 
+          class="w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300" 
+        />
+      </div>
+      <div>
+        <label for="message">Message *</label>
+        <textarea id="message" name="message" rows="3" 
+          class="w-full rounded-md border-0 py-2 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"
+        />
+      </div>
+      <button type="submit" 
+        class="rounded-md bg-green-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600">
+        SEND
+      </button>
+    </div>
+  </form>
+</template>
