bc858d239467446c5874037b7bfaff2639aaef90
Monika Mráziková <monika.monika.mrazikova@gmail.com>
2023-05-15 17:20:17 +0200

    Link to home changed

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="bg-gray-900 text-gray-100 py-3.5 px-6 shadow md:flex justify-between items-center">
-    <div class="cursor-pointer">
-      <h1 class="text-xl">Alpaq</h1>
-    </div>
+    <a :href="home" class="text-xl">Alpaq</a>
     <ul class="md:flex md:items-center">
       <li v-for="link in links" v-bind:key="link" class="md:mx-4">
         <a :href="link.link" class="text-xl hover:text-green-500">{{ link.name }}</a>
@@ -15,8 +13,8 @@
 export default {
   data() {
     return {
+      home: "/",
       links: [
-        { name: "Home", link: "/" },
         { name: "Gallery", link: "/gallery" },
         { name: "News", link: "/news" },
         { name: "About", link: "/about" },
